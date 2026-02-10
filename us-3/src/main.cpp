760edb1c2a5da742cee84e5f591ab4b4a4afaa33
Antony Dias <antony.overwatch@gmail.com>
2026-02-10 16:04:55 +0100

    add light sensor reading and display to LCD

--- us-3/src/main.cpp
+++ us-3/src/main.cpp
@@ -4,6 +4,7 @@
 
 #define DHTTYPE DHT11 // DHT 11
 #define DHTPIN 2
+#define LDRPIN A0 // Light Dependent Resistor on analog pin A0
 
 #if defined(ARDUINO_ARCH_AVR)
 #define debug Serial
@@ -51,24 +52,35 @@ void loop()
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
 
+  // Read LDR value and convert to lux
+  int ldrValue = analogRead(LDRPIN);
+  // Calibrate these values based on your LDR and lighting conditions
+  // Typical: dark = ~1023, bright = ~0 (inverse relationship)
+  int lightLux = map(ldrValue, 0, 1023, 0, 10000); // maps to 0-10000 lux
+
   if (!dht.readTempAndHumidity(temp_hum_val))
   {
+    // Display light level and temperature on LCD
     lcd.setCursor(0, 0);
-    lcd.print("Hum: ");
-    lcd.print(temp_hum_val[0]);
-    lcd.print(" %");
+    lcd.print("Lum: ");
+    lcd.print(lightLux);
+    lcd.print(" lux");
 
     lcd.setCursor(0, 1);
     lcd.print("Temp: ");
     lcd.print(temp_hum_val[1]);
     lcd.print(" *C");
 
+    // Print all data to console
     debug.print("Humidity: ");
     debug.print(temp_hum_val[0]);
     debug.print(" %\t");
     debug.print("Temperature: ");
     debug.print(temp_hum_val[1]);
-    debug.println(" *C");
+    debug.print(" *C\t");
+    debug.print("Light Level: ");
+    debug.print(lightLux);
+    debug.println(" lux");
   }
   else
   {
